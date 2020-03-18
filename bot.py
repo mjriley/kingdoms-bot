17e1f7fa1793b4c49c4d06aa1c5d74cc51cac2e8
Matthew Riley <mjriley2@gmail.com>
2020-03-18 01:30:36 -0700

    Removed dotenv dependency

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ import discord
 import random
 from discord.ext import commands
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = commands.Bot(command_prefix='!')
